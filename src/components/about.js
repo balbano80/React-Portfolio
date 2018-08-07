1ba460999a64043f3605e5c6959fb4d78e9a1acd
balbano80 <balbano80@gmail.com>
2018-08-06 17:37:35 -0700

    copied over from other repo to enable github pages option

---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/about.css';
+
+
+const About = () => (
+    <div>
+        <h1> About Page </h1>
+    </div>
+);
+
+export default About;
